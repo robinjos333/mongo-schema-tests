4031783f0a6a57287ef3d6e3c8a33c39421891a1
robinjos333 <robinjos333@gmail.com>
2026-06-26 13:12:04 +0530

    remove subdocument

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -12,4 +12,32 @@ describe("Subdouments", () => {
     const user = await User.findOne({ name: "robin" });
     assert(user.posts[0].title === "my first post");
   });
+  it("can add subdouments to an existing record", async () => {
+    const abin = new User({
+      name: "Abin",
+      posts: [],
+    });
+    await abin.save();
+    const user = await User.findOne({ name: "Abin" });
+    user.posts.push({ title: "my second post" });
+    await user.save();
+
+    const updatedUser = await User.findOne({ name: "Abin" });
+    assert(updatedUser.posts[0].title === "my second post");
+  });
+
+  it("can can remove subdouments to an existing record", async () => {
+    const savin = new User({
+      name: "Savin",
+      posts: [{ title: "new title" }],
+    });
+    await savin.save();
+    const user = await User.findOne({ name: "Savin" });
+    // user.posts.pull(user.posts[0]); // -- old method
+    user.posts[0].deleteOne();
+    await user.save();
+
+    const updatedUser = await User.findOne({ name: "Savin" });
+    assert(updatedUser.posts.length === 0);
+  });
 });
