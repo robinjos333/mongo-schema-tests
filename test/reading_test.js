861565cd5eef5c81ff4d1e20422295ec4aa0b665
Robin J Njarekkadan <robin.j.njarekkadan@ibm.com>
2026-06-30 15:25:46 +0530

    added sort skip limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,14 +2,17 @@ const User = require("../src/users");
 const assert = require("assert");
 
 describe("reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
   beforeEach(async () => {
     await User.deleteMany({});
 
     await User.syncIndexes();
 
     joe = new User({ name: "Joe" });
-    await joe.save();
+    maria = new User({ name: "Maria" });
+    alex = new User({ name: "Alex" });
+    zach = new User({ name: "Ach" });
+    await Promise.all([joe.save(), maria.save(), alex.save(), zach.save()]);
   });
 
   it("find all the users with a name of joe", async () => {
@@ -21,5 +24,9 @@ describe("reading users out of the database", () => {
     const user = await User.findOne({ name: "Joe" });
     assert(user !== null);
     assert(user._id.toString() === joe._id.toString());
+  });
+  it("can skip and limit the result set", async () => {
+    const users = await User.find({}).sort({ name: 1 }).skip(1).limit(2);
+    assert(users.length === 2);
   });
 });
